3c1ad4e017c10f22e241d9ba528f94efafb17d86
Rana Sarkar <rana.sarkarg3@gmail.com>
2022-03-26 01:19:46 +0530

    router
    router

--- c3-evaluation/src/App.jsx
+++ c3-evaluation/src/App.jsx
@@ -1,23 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Home } from './components/Home/Home';
-import { Navbar } from './components/Navbar/Navbar';
-import { HistorySection } from './components/Section/HistorySection';
-import { MysterySection } from './components/Section/MysterySection';
-import { MythologySection } from './components/Section/MythologySection';
-import { TechnologySection } from './components/Section/TechnologySection';
+import { AllRoutes } from './components/Section/AllRoutes';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} ></Route>
-        <Route path="/mystery" element={<MysterySection />} ></Route>
-        <Route path="/technology" element={<TechnologySection />} ></Route>
-        <Route path="/mythology" element={<MythologySection />} ></Route>
-        <Route path="/history" element={<HistorySection/>} ></Route>
-      </Routes>
+        <AllRoutes/>
     </div>
   );
 }
